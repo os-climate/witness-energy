ce01c300e44a5ddf1b64db466c7b6754e5620720
Julien Souchard <NG92D02@eu.airbus.corp>
2022-08-03 13:55:23 +0200

    Try energy_jacobian_dump script without parallel

--- energy_models/tests/energy_model_jacobian_pkl_dump.py
+++ energy_models/tests/energy_model_jacobian_pkl_dump.py
@@ -6,11 +6,15 @@ Created on 10 May 2021
 import energy_models.tests as jacobian_target
 
 from sos_trades_core.tests.core.abstract_jacobian_unit_test import AbstractJacobianUnittest
-
+from os import listdir
 
 if __name__ == '__main__':
-    AbstractJacobianUnittest.launch_all_pickle_generation(
-        jacobian_target, 'l1_*.py')
+
+    test_files_list=[file for file in listdir() if file.startswith('l1_test_gradient')]
+
+    for test_file in test_files_list:
+        AbstractJacobianUnittest.launch_all_pickle_generation(
+            jacobian_target, test_file)
 
     #AbstractJacobianUnittest.launch_all_pickle_generation(
     #    jacobian_target, '_l1_test_gradient_energy_mix.py')
