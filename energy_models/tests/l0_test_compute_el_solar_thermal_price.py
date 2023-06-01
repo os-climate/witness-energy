0f0e71c20b49a3f38455b4d2f0abfbb9dbf08bb6
Carlos Ortega <carlos.ortega-absil@capgemini.com>
2023-06-01 11:37:31 +0200

    commenting out show graphs

--- energy_models/tests/l0_test_compute_el_solar_thermal_price.py
+++ energy_models/tests/l0_test_compute_el_solar_thermal_price.py
@@ -223,8 +223,8 @@ class SolarThermalPriceTestCase(unittest.TestCase):
         filters = disc.get_chart_filter_list()
         graph_list = disc.get_post_processing_list(filters)
 
-        for graph in graph_list:
-            graph.to_plotly().show()
+        # for graph in graph_list:
+        #     graph.to_plotly().show()
 
 # if __name__ == "__main__":
 #     unittest.main()
