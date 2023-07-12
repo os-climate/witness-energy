974d8b1ca7d75489c12f186223db7060e6ed2469
fadwaouniss <fadwa.ouniss@capgemini.com>
2023-07-12 11:34:26 +0200

    Extracting Capex, Opex, CO2_Tax and total price

--- energy_models/sos_processes/post_processing/techno_price.py
+++ energy_models/sos_processes/post_processing/techno_price.py
@@ -11,6 +11,10 @@ def get_comparision_data(execution_engine, namespace, year):
     var_f_name = f"{namespace}.technologies_list"
     techno_list = execution_engine.dm.get_value(var_f_name)
 
+    if techno_list is None:
+        # Managing the case where the list of technologies is None
+        raise ValueError("La liste des technologies est manquante ou vide.")
+
     capex_list = []
     opex_list = []
     energy_costs_List = []
@@ -34,10 +38,34 @@ def get_comparision_data(execution_engine, namespace, year):
     table = InstanciatedTable('Data Comparison for Year ' , headers, cells)
     return table
 
-def get_graphs(table, title):
+# def get_graphs(table, title):
 #     '''
 #     Table chart where Capex, Opex and total price
 #     '''
 #
 #
-     return new_chart
+#     return new_chart
+
+
+
+
+
+# def dummy_price_data(techno):
+#     # Ici, tu peux renvoyer des données factices pour les prix détaillés d'une technologie
+#     return {
+#         'CAPEX_Part': [100, 200, 300],
+#         'OPEX_Part': [10, 20, 30],
+#         techno: ['a', 'b', 'c']
+#     }
+#
+# execution_engine = ['Tech1', 'Tech2', 'Tech3']
+# namespace =['a','b','c']
+# year = 2023
+#
+# # Étape 3 : Appel de la fonction get_comparision_data
+# result = get_comparision_data(execution_engine, namespace, year)
+#
+# # Étape 4 : Affichage de la table
+# table_data = result.get_table_data()  # Obtention des données de la table
+# df = pd.DataFrame(table_data[1:], columns=table_data[0])  # Conversion en DataFrame pandas
+# print(df)
