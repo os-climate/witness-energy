aeba45ded600d773f1573547c8b6ec4d978d0cca
KAJBORKA <128482032+KAJBORKA@users.noreply.github.com>
2023-08-14 18:51:48 +0530

    Created process and usecase for low and medium heat.

---
+++ energy_models/sos_processes/energy/techno_mix/medium_heat_mix/process.py
@@ -0,0 +1,67 @@
+'''
+Copyright 2022 Airbus SAS
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+#-- generate coupling between:
+#    - ac_model
+#    - economics_operator
+
+from energy_models.core.energy_process_builder import EnergyProcessBuilder,\
+    INVEST_DISCIPLINE_OPTIONS
+from energy_models.core.stream_type.energy_models.heat import mediumtemperatureheat
+from energy_models.sos_processes.energy.techno_mix.medium_heat_mix.usecase import TECHNOLOGIES_LIST
+
+
+class ProcessBuilder(EnergyProcessBuilder):
+
+    # ontology information
+    _ontology_data = {
+        'label': 'Energy Technology Mix - Medium Heat Mix',
+        'description': '',
+        'category': '',
+        'version': '',
+    }
+
+    def __init__(self, ee):
+        EnergyProcessBuilder.__init__(self, ee)
+        self.techno_list = TECHNOLOGIES_LIST
+
+    def get_builders(self):
+
+        ns_study = self.ee.study_name
+
+        heat_name = mediumtemperatureheat.name
+        energy_mix = 'EnergyMix'
+        ns_dict = {'ns_heat': f'{ns_study}.{energy_mix}.{heat_name}',
+                   'ns_energy': f'{ns_study}.{energy_mix}',
+                   'ns_energy_study': f'{ns_study}',
+                   'ns_public': f'{ns_study}', 'ns_resource': f'{ns_study}.{energy_mix}'}
+
+        mods_dict = {}
+        mods_dict[f'{energy_mix}.{heat_name}'] = self.get_stream_disc_path(
+            'energy_disciplines', 'Heat')
+        for techno_name in self.techno_list:
+            mods_dict[f'{energy_mix}.{heat_name}.{techno_name}'] = self.get_techno_disc_path(
+                heat_name, techno_name)
+
+        builder_list = self.create_builder_list(mods_dict, ns_dict=ns_dict, associate_namespace=self.associate_namespace)
+        if self.invest_discipline == INVEST_DISCIPLINE_OPTIONS[0]:
+            mods_dict_invest = {f'{energy_mix}.{heat_name}': 'energy_models.core.investments.disciplines.techno_invest_disc.InvestTechnoDiscipline',
+                                }
+
+            builder_list_invest = self.create_builder_list(
+                mods_dict_invest, ns_dict=ns_dict, associate_namespace = self.associate_namespace)
+
+            builder_list.extend(builder_list_invest)
+        return builder_list
