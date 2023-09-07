93bd663d6b882f6b62668bd95add2110e98375c1
KAJBORKA <128482032+KAJBORKA@users.noreply.github.com>
2023-09-07 18:00:44 +0530

    Remove 1 level of abstraction from the tree (high, medium, low) as it was duplicate one.

--- energy_models/core/stream_type/energy_models/heat.py
+++ energy_models/core/stream_type/energy_models/heat.py
@@ -18,7 +18,7 @@ from energy_models.core.stream_type.energy_type import EnergyType
 
 
 class lowtemperatureheat(EnergyType):
-    name = 'heat' + '.' + 'low' + '.' + 'lowheattemperature'
+    name = 'heat' + '.' + 'lowheattemperature'
     short_name = 'low heat'
     default_techno_list = ['NaturalGasBoiler', 'ElectricBoiler', 'HeatPump', 'Geothermal']
     data_energy_dict = {'maturity': 5,
@@ -29,7 +29,7 @@ class lowtemperatureheat(EnergyType):
 
 class mediumtemperatureheat(EnergyType):
 
-    name = 'heat' + '.' + 'medium' + '.' + 'mediumheattemperature'
+    name = 'heat' + '.' + 'mediumheattemperature'
     short_name = 'medium heat'
     default_techno_list = ['NaturalGasBoiler', 'ElectricBoiler', 'HeatPump', 'Geothermal']
     data_energy_dict = {'maturity': 5,
@@ -41,7 +41,7 @@ class mediumtemperatureheat(EnergyType):
 
 class hightemperatureheat(EnergyType):
 
-    name = 'heat' + '.' + 'high' + '.' + 'highheattemperature'
+    name = 'heat' + '.' + 'highheattemperature'
     short_name = 'high heat'
     default_techno_list = ['NaturalGasBoiler', 'ElectricBoiler', 'HeatPump', 'Geothermal']
     data_energy_dict = {'maturity': 5,
