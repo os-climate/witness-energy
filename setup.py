905f0398d81481f5b37a8623660ffec715f11935
ggoyon <guillaume.goyon@capgemini.com>
2023-11-02 17:33:17 +0100

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -36,6 +36,10 @@ with open('README.md') as f:
 with open('LICENSE') as f:
     license = f.read()
 
+# Read the requirements.in file and extract the required dependencies
+with open('requirements.in', 'r') as requirements_file:
+    reqs_list = [line.strip() for line in requirements_file if line.strip()]
+
 
 # Manage module version using date
 today = date.today()
@@ -60,14 +64,11 @@ setup(
     version=version,
     description='Python library to define the energy price and supply for each environmental scenarios',
     long_description=readme,
-    author='Airbus SAS',
-    url='https://idlvsrv284.eu.airbus.corp/sostrade/energy_simple.git',
+    author='Airbus SAS, Capgemini',
+    url='https://github.com/os-climate/witness-energy',
     license=license,
     packages=find_packages(exclude=('tests', 'docs')),
     include_package_data=True,
-    python_requires='>=3.7',
-    install_requires=[
-        'nose2>= 0.9.1',
-        'sos-trades-core'
-    ]
+    python_requires='>=3.9',
+    install_requires=reqs_list,
 )
