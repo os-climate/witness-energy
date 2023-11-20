3a84e6b6a18b6efa9bc10e6489dc36f4055076c2
KAJBORKA <128482032+KAJBORKA@users.noreply.github.com>
2023-11-20 05:56:50 +0530

    imported glossary core in data generator file.

--- energy_models/tests/data_tests/mda_energy_data_generator.py
+++ energy_models/tests/data_tests/mda_energy_data_generator.py
@@ -18,6 +18,8 @@ limitations under the License.
 
 from sostrades_core.execution_engine.execution_engine import ExecutionEngine
 from energy_models.sos_processes.energy.MDA.energy_process_v0.usecase import Study as MDA_Energy
+from climateeconomics.glossarycore import GlossaryCore as GlossaryCore
+
 import pickle
 
 
@@ -63,6 +65,7 @@ def launch_data_pickle_generation(directory=''):
     ############
     # Energies #
     ############
+    # energy_list.remove("biomass_dry")
     for energy in energy_list:
         # Loop on energies
         energy_disc = ee.dm.get_disciplines_with_name(
