371fcf111ccb7e45880a832b1116fd5fd3221218
perrotcap <antoine.perrot@capgemini.com>
2023-11-23 10:58:46 +0100

    deleted dead code & update headers

--- energy_models/tests/l0_test_disc_double_run.py
+++ energy_models/tests/l0_test_disc_double_run.py
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
 '''
 Copyright 2022 Airbus SAS
+Modifications on 23/11/2023 Copyright 2023 Capgemini
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
