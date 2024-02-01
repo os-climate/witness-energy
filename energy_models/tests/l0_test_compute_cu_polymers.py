48aad88080275bfbba2c7d1b83c9162643eaa898
KAJBORKA <128482032+KAJBORKA@users.noreply.github.com>
2024-02-01 23:49:38 +0530

    Created new technologies in carbon utilization

--- energy_models/tests/l0_test_compute_cu_polymers.py
+++ energy_models/tests/l0_test_compute_cu_polymers.py
@@ -1,5 +1,6 @@
 '''
-Copyright 2023 Capgemini
+Copyright 2022 Airbus SAS
+Modifications on 2023/11/07-2023/11/16 Copyright 2023 Capgemini
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -13,9 +14,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 '''
-
 import unittest
-from os.path import join, dirname
 
 import numpy as np
 import pandas as pd
@@ -26,46 +25,46 @@ from energy_models.glossaryenergy import GlossaryEnergy
 from sostrades_core.execution_engine.execution_engine import ExecutionEngine
 
 
-class CHPTestCase(unittest.TestCase):
+class PolymersPriceTestCase(unittest.TestCase):
     """
-    CHP test class
+    Polymers prices test class
     """
 
     def setUp(self):
         '''
         Initialize third data needed for testing
         '''
-        years = np.arange(GlossaryEnergy.YeartStartDefault, 2050 + 1)
+        years = np.arange(2020, 2051)
         self.resource_list = [
             'oil_resource', 'natural_gas_resource', 'uranium_resource', 'coal_resource']
         self.ratio_available_resource = pd.DataFrame(
-            {GlossaryEnergy.Years: np.arange(GlossaryEnergy.YeartStartDefault, 2050 + 1)})
+            {GlossaryEnergy.Years: np.arange(2020, 2050 + 1)})
         for types in self.resource_list:
             self.ratio_available_resource[types] = np.linspace(
                 1, 1, len(self.ratio_available_resource.index))
+        self.energy_carbon_emissions = pd.DataFrame(
+            {GlossaryEnergy.Years: years, 'CO2': 0})
+        self.invest_level_2 = pd.DataFrame(
+            {GlossaryEnergy.Years: years, GlossaryEnergy.InvestValue: np.ones(len(years)) * 0.0325})
 
-        self.energy_prices = pd.DataFrame({GlossaryEnergy.Years: years,
-                                           'methane': np.ones(len(years)) * 100,  #($/MWh)
-                                           })
-
-        self.energy_carbon_emissions = pd.DataFrame({GlossaryEnergy.Years: years, 'electricity': 0.0, 'methane': 0.0})
-        self.resources_price = pd.DataFrame({GlossaryEnergy.Years: years, 'water_resource': 2.0})
-        self.resources_CO2_emissions = pd.DataFrame({GlossaryEnergy.Years: years, 'water': 0.0})
-        self.invest_level = pd.DataFrame(
-            {GlossaryEnergy.Years: years, GlossaryEnergy.InvestValue: np.ones(len(years)) * 10.0})
         co2_taxes_year = [2018, 2020, 2025, 2030, 2035, 2040, 2045, 2050]
-        co2_taxes = [14.86, 17.22, 20.27,
-                     29.01, 34.05, 39.08, 44.69,   50.29]
+        # co2_taxes = [0.01486, 0.01722, 0.02027,
+        #             0.02901,  0.03405,   0.03908,  0.04469,   0.05029]
+        co2_taxes = [0, 0, 0,
+                     0,  0,   0,  0,   0]
         func = sc.interp1d(co2_taxes_year, co2_taxes,
                            kind='linear', fill_value='extrapolate')
 
         self.co2_taxes = pd.DataFrame(
             {GlossaryEnergy.Years: years, GlossaryEnergy.CO2Tax: func(years)})
         self.margin = pd.DataFrame(
-            {GlossaryEnergy.Years: years, GlossaryEnergy.MarginValue: np.ones(len(years)) * 110.0})
-        # From future of hydrogen
+            {GlossaryEnergy.Years: years, GlossaryEnergy.MarginValue: np.ones(len(years)) * 100.0})
+
+        transport_cost = 0
+
         self.transport = pd.DataFrame(
-            {GlossaryEnergy.Years: years, 'transport': np.ones(len(years)) * 0})
+            {GlossaryEnergy.Years: years, 'transport': np.ones(len(years)) * transport_cost})
+        self.resources_price = pd.DataFrame({GlossaryEnergy.Years: years})
         self.scaling_factor_techno_consumption = 1e3
         self.scaling_factor_techno_production = 1e3
         demand_ratio_dict = dict(
@@ -75,47 +74,44 @@ class CHPTestCase(unittest.TestCase):
         self.is_stream_demand = True
         self.is_apply_resource_ratio = True
 
-        biblio_data_path = join(
-            dirname(__file__), 'output_values_check', 'biblio_data.csv')
-        self.biblio_data = pd.read_csv(biblio_data_path)
-        self.biblio_data = self.biblio_data.loc[self.biblio_data['sos_name']
-                                                == 'methane.CHP']
-
     def tearDown(self):
         pass
 
-    def test_02_chp_discipline(self):
+    def test_01_polymers_discipline(self):
 
         self.name = 'Test'
-        self.model_name = 'CHP'
+        self.model_name = 'Polymers'
         self.ee = ExecutionEngine(self.name)
-        ns_dict = {'ns_public': self.name, 'ns_energy': f'{self.name}',
-                   'ns_energy_study': f'{self.name}',
-                   'ns_methane': f'{self.name}',
-                   'ns_resource': self.name,
-                   'ns_heat_medium': f'{self.name}'
-                   }
+        ns_dict = {'ns_public': self.name,
+                   'ns_energy': self.name,
+                   'ns_energy_study': self.name,
+                   'ns_electricity': self.name,
+                   'ns_carbon_utilization': self.name,
+                   'ns_resource': self.name}
         self.ee.ns_manager.add_ns_def(ns_dict)
 
-        mod_path = 'energy_models.models.heat.medium.chpmediumheat.chpmediumheat_disc.CHPMediumHeatDiscipline'
+        mod_path = 'energy_models.models.carbon_utilization.polymers.polymers_disc.PolymersDiscipline'
         builder = self.ee.factory.get_builder_from_module(
             self.model_name, mod_path)
 
         self.ee.factory.set_builders_to_coupling_builder(builder)
 
-        self.ee.configure()
+        import traceback
+        try:
+            self.ee.configure()
+        except:
+            traceback.print_exc()
         self.ee.display_treeview_nodes()
 
         inputs_dict = {f'{self.name}.{GlossaryEnergy.YearEnd}': 2050,
-                       f'{self.name}.{GlossaryEnergy.EnergyPricesValue}': self.energy_prices,
-                       f'{self.name}.{GlossaryEnergy.ResourcesPriceValue}': self.resources_price,
+                       f'{self.name}.{GlossaryEnergy.EnergyPricesValue}': pd.DataFrame({GlossaryEnergy.Years: np.arange(2020, 2051)}),
                        f'{self.name}.{GlossaryEnergy.EnergyCO2EmissionsValue}': self.energy_carbon_emissions,
-                       f'{self.name}.{self.model_name}.{GlossaryEnergy.InvestLevelValue}': self.invest_level,
+                       f'{self.name}.{self.model_name}.{GlossaryEnergy.InvestLevelValue}': self.invest_level_2,
                        f'{self.name}.{GlossaryEnergy.CO2TaxesValue}': self.co2_taxes,
                        f'{self.name}.{GlossaryEnergy.TransportMarginValue}': self.margin,
                        f'{self.name}.{GlossaryEnergy.TransportCostValue}': self.transport,
-                       f'{self.name}.{self.model_name}.{GlossaryEnergy.MarginValue}':  self.margin
-                       }
+                       f'{self.name}.{GlossaryEnergy.ResourcesPriceValue}': self.resources_price,
+                       f'{self.name}.{self.model_name}.{GlossaryEnergy.MarginValue}':  self.margin}
 
         self.ee.load_study_from_input_dict(inputs_dict)
 
@@ -125,9 +121,5 @@ class CHPTestCase(unittest.TestCase):
             f'{self.name}.{self.model_name}')[0]
         filters = disc.get_chart_filter_list()
         graph_list = disc.get_post_processing_list(filters)
-        for graph in graph_list:
-            graph.to_plotly().show()
-
-
-if __name__ == "__main__":
-    unittest.main()
+        # for graph in graph_list:
+        #     graph.to_plotly().show()
