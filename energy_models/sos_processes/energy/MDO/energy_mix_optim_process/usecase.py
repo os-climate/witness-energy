ee45a50cbe9fbbb5c2b75a6e6dd44671c6b9200d
perrotcap <antoine.perrot@capgemini.com>
2024-02-12 10:48:35 +0100

    fixed l0 test & headers

--- energy_models/sos_processes/energy/MDO/energy_mix_optim_process/usecase.py
+++ energy_models/sos_processes/energy/MDO/energy_mix_optim_process/usecase.py
@@ -1,27 +1,21 @@
 '''
 Copyright 2024 Capgemini
-Licensed under the Apache License, Version 2.0 (the 'License');
+Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an 'AS IS' BASIS,
+distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 
 '''
-import numpy as np
-import pandas as pd
-
 from energy_models.core.energy_study_manager import DEFAULT_TECHNO_DICT
 from energy_models.sos_processes.energy.MDA.energy_mix_optim_sub_process.usecase import Study as subStudy
-from energy_models.glossaryenergy import GlossaryEnergy
-from sostrades_core.execution_engine.func_manager.func_manager import FunctionManager
 from sostrades_core.execution_engine.func_manager.func_manager_disc import FunctionManagerDisc
-from sostrades_core.study_manager.base_study_manager import BaseStudyManager
 from sostrades_core.study_manager.study_manager import StudyManager
 
 INVEST_DISC_NAME = 'InvestmentDistribution'
