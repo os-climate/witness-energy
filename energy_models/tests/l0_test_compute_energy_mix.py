3bdce6636c03ff6ac86462c2bc735dd1794ce81b
perrotcap <antoine.perrot@capgemini.com>
2024-02-12 11:03:22 +0100

    commented plot graph

--- energy_models/tests/l0_test_compute_energy_mix.py
+++ energy_models/tests/l0_test_compute_energy_mix.py
@@ -546,7 +546,7 @@ class EnergyMixTestCase(unittest.TestCase):
             disc, filters, as_json=False)
         for graph in graph_list:
             pass
-            graph.to_plotly().show()
+            #graph.to_plotly().show()
 
     def test_04_energy_mix_resource(self):
         """
