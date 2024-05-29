8d56dc744f7530c0b13c764179b8615cf0b8fcde
carlalabarthe <carla.labarthe@capgemini.com>
2024-05-29 10:37:00 +0200

    modified the header by Capgemini header

--- energy_models/tests/l1_test_gradient_investments_profile_builder.py
+++ energy_models/tests/l1_test_gradient_investments_profile_builder.py
@@ -1,12 +1,10 @@
 '''
-Copyright 2022 Airbus SAS
-Modifications on 2023/11/07-2023/11/09 Copyright 2023 Capgemini
-
+Copyright 2024 Capgemini
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
