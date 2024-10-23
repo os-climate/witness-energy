bce89eab82cadba60344714e93a7a7230d1017d6
benherry <benjamin.herry@capgemini.com>
2024-10-23 15:27:39 +0200

    feat: impose utilisation ratio=100, learning rate = 0 and age_distrib=1 to simplify the problem

--- data_energy/fitting/fossil_energy_simple_techno.py
+++ data_energy/fitting/fossil_energy_simple_techno.py
@@ -28,13 +28,14 @@ from energy_models.models.fossil.fossil_simple_techno.fossil_simple_techno_disc 
     FossilSimpleTechnoDiscipline,
 )
 
+year_calibration = 2015
+
 df_invest_historic = DatabaseWitnessEnergy.get_techno_invest_df(techno_name=GlossaryEnergy.FossilSimpleTechno)
 df_prod_historic = DatabaseWitnessEnergy.get_techno_prod(techno_name=GlossaryEnergy.FossilSimpleTechno, year=2020)[1].value
-#ToDo: with or without tax?
-ref_price_2023 = 121.5 # $/MWh Source: chatgpt
+ref_price_2023 = 121.5 # $/MWh Source: chatgpt LCOE without tax
 # data to run techno
 construction_delay = GlossaryEnergy.TechnoConstructionDelayDict[GlossaryEnergy.FossilSimpleTechno]
-year_start_fitting = int(max(df_invest_historic['years'].min() + construction_delay, df_prod_historic['years'].min(), 2020))
+year_start_fitting = int(max(df_invest_historic['years'].min() + construction_delay, df_prod_historic['years'].min(), year_calibration))
 year_end_fitting = int(min(df_invest_historic['years'].max(), df_prod_historic['years'].max()))
 
 prod_values_historic = df_prod_historic.loc[(df_prod_historic['years'] >= year_start_fitting) & (df_prod_historic['years'] <= year_end_fitting)]['production'].values
@@ -71,10 +72,10 @@ ee.display_treeview_nodes()
 def run_model(x: list, year_end: int = year_end_fitting):
     techno_dict_default["Capex_init"] = x[0]
     init_age_distrib_factor = x[1]
-    #techno_dict_default["learning_rate"] = x[2]
+    techno_dict_default["learning_rate"] = x[2]
     techno_dict_default["Opex_percentage"] = x[3]
     techno_dict_default["WACC"] = x[4]
-    utilisation_ratio = pd.DataFrame({GlossaryEnergy.Years: np.arange(year_start_fitting, year_end + 1),
+    utilisation_ratio = pd.DataFrame({GlossaryEnergy.Years: years_fitting,
                                                           GlossaryEnergy.UtilisationRatioValue: x[5:]})
 
     inputs_dict = {
@@ -112,11 +113,11 @@ def fitting_renewable(x: list):
 
 
 # Initial guess for the variables
-years = np.arange(year_start_fitting, year_end_fitting + 1)
-x0 = np.concatenate((np.array([100., 1., 0.0, 0.024, 0.058]), 100.0 * np.ones_like(years))) # [capex_init, init_age_distrib_factor, learnin_rate, Opex_fraction, WACC]
-#x0 = np.array([743.8, 1.3, 0.06, 0.0, 0.06])
+# [capex_init, init_age_distrib_factor, learnin_rate, Opex_fraction, WACC, utilization_ratio]
+x0 = np.concatenate((np.array([200., 1., 0.0, 0.024, 0.058]), 100.0 * np.ones_like(years_fitting)))
 
-bounds = [(0, 10000), (0, 3.0), (0.01, 0.95), (0.001, 0.99), (0.0001, 0.3)] + len(years) * [(0.1, 100.0)]
+# can put different lower and upper bounds for utilization ratio if want to activate it
+bounds = [(0, 10000), (1.0, 1.0), (0.0, 0.0), (0.001, 0.99), (0.0001, 0.3)] + len(years_fitting) * [(100.0, 100.0)]
 
 # Use minimize to find the minimum of the function
 result = minimize(fitting_renewable, x0, bounds=bounds)
@@ -124,7 +125,6 @@ result = minimize(fitting_renewable, x0, bounds=bounds)
 prod_values_model, price_model_values = run_model(result.x)
 
 # Print the result
-#print("Optimal solution:", result.x)
 print("Function value at the optimum:", result.fun)
 
 
@@ -140,13 +140,13 @@ new_chart.series.append(serie)
 
 new_chart.to_plotly().show()
 
-parameters = ["capex_init", "init_age_distrib_factor", "learning_rate", "opex_percentage", "wacc", "utilization_ratio"]
-opt_values = dict(zip(parameters, np.round(result.x, 2)))
-for key, val in opt_values.items():
-    print("Optimal", key, ":", val)
-
 capex_init, init_age_distrib_factor, learning_rate, opex_percentage, wacc = result.x[0:5]
 utilization_ratio = result.x[5:]
+parameters = ["capex_init", "init_age_distrib_factor", "learning_rate", "opex_percentage", "wacc"]
+opt_values = dict(zip(parameters, np.round(result.x, 3)))
+for key, val in opt_values.items():
+    print("Optimal", key, ":", val)
+print("Optimal utilization_ratio", utilization_ratio)
 
 disc = ee.dm.get_disciplines_with_name(
             f'{name}.{model_name}')[0]
